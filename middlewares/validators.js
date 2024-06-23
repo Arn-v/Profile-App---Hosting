d5d5fd9d942618378ee6518b7756f88c11938ade
Arn-v <singharinav69@gmail.com>
2024-06-23 17:49:36 +0530

    adding validation

---
+++ middlewares/validators.js
@@ -0,0 +1,23 @@
+const { check, validationResult } = require('express-validator');
+
+exports.validateUserProfile = [
+    check('profileData.firstName')
+        .notEmpty().withMessage('First name is required')
+        .isLength({ max: 50 }).withMessage('First name can be at most 50 characters long'),
+    check('profileData.lastName')
+        .notEmpty().withMessage('Last name is required')
+        .isLength({ max: 60 }).withMessage('Last name can be at most 60 characters long'),
+    check('profileData.email')
+        .notEmpty().withMessage('Email is required')
+        .isEmail().withMessage('Email must be valid'),
+    check('profileData.address')
+        .notEmpty().withMessage('Address is required')
+        .isLength({ max: 200 }).withMessage('Address can be at most 200 characters long'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ success: false, errors: errors.array() });
+        }
+        next();
+    }
+];
--- routes/Profile.js
+++ routes/Profile.js
@@ -1,13 +1,15 @@
 const { getUserProfile, saveProfile } = require('../controllers/ProfileController')
 const { auth } = require('../middlewares/auth');
 
+
 const express = require("express") ; 
+const { validateUserProfile } = require('../middlewares/validators');
 
 const router = express.Router() ;
 
 
 router.get("/" , getUserProfile )  ; 
-router.post("/save" , saveProfile) ; 
+router.post("/save" , validateUserProfile ,saveProfile) ; 
 
 
 module.exports = router ;  
