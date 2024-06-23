553b9691cabb355f3681bc9a53ea85e679a3ff32
Arn-v <singharinav69@gmail.com>
2024-06-23 17:59:08 +0530

    change

--- routes/Profile.js
+++ routes/Profile.js
@@ -1,12 +1,12 @@
 const { getUserProfile, saveProfile } = require('../controllers/ProfileController')
 const { auth } = require('../middlewares/auth');
+const { validateUserProfile } = require('../middlewares/validators');
 
 
 const express = require("express") ; 
-const { validateUserProfile } = require('../middlewares/validators');
+
 
 const router = express.Router() ;
-
 
 router.get("/" , getUserProfile )  ; 
 router.post("/save" , validateUserProfile ,saveProfile) ; 
